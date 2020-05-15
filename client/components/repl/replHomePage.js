0a5a42191d6c5602eed7f78c898080c9ef71e744
Wilda Cheng <wildacheng5@gmail.com>
2020-05-15 01:10:37 -0400

    homePage CSS

--- client/components/repl/replHomePage.js
+++ client/components/repl/replHomePage.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import './replHomePage.css'
 
 class HomePage extends Component {
   constructor(props) {
@@ -44,38 +45,44 @@ class HomePage extends Component {
 
     return (
       <div>
-        <form>
-          <label htmlFor="name">Name:</label>
-          <input
-            onChange={this.handleChange}
-            type="text"
-            id="name"
-            name="name"
-            value={name}
-          />
-          <label htmlFor="name">Room:</label>
-          <input
-            onChange={this.handleChange}
-            type="text"
-            id="room"
-            name="roomName"
-            value={roomName}
-          />
-          <br />
-
-          <input
-            onClick={this.joinRoom}
-            type="submit"
-            value="Join Room"
-          ></input>
-          <input
-            onClick={this.createRoom}
-            type="submit"
-            name="roomName"
-            value="Create Room"
-          ></input>
-        </form>
-        <hr />
+        <p id="header">
+          <a id="inHeader">REPLroom</a>
+        </p>
+        <div className="login">
+          <form>
+            <label htmlFor="name">Name</label>
+            <input
+              onChange={this.handleChange}
+              type="text"
+              id="name"
+              name="name"
+              value={name}
+            />
+            <label htmlFor="name">Room</label>
+            <input
+              onChange={this.handleChange}
+              type="text"
+              id="room"
+              name="roomName"
+              value={roomName}
+            />
+            <br />
+            <input
+              id="joinBtn"
+              className="button"
+              onClick={this.joinRoom}
+              type="submit"
+              value="Join Room"
+            ></input>
+            <input
+              className="button"
+              onClick={this.createRoom}
+              type="submit"
+              name="roomName"
+              value="Create Room"
+            ></input>
+          </form>
+        </div>
       </div>
     )
   }
