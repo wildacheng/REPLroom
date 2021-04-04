56e9d669c6c03dbb79dc0f1e683a453d3d4936c5
Wilda Cheng <wildacheng5@gmail.com>
2021-04-04 00:40:38 -0400

    try

--- server/db/db.js
+++ server/db/db.js
@@ -7,10 +7,10 @@ const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
     logging: false,
-    ssl: true,
-    dialectOptions: {
-      ssl: true
-    },
+    // ssl: true,
+    // dialectOptions: {
+    //   ssl: true
+    // },
   }
 )
 module.exports = db
